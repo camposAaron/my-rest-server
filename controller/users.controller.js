f6e130ea3b4d943347e734e95898961680428090
jos_aa <jos_aaron@hotmail.com>
2021-08-03 21:22:20 -0600

    validaciones de usuarios hechas

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const {response, request } = require('express');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 
 const getUsers = (req = request, res = response) => {
@@ -24,16 +25,24 @@ const putUsers = (req, res) => {
 
 const postUsers = async(req, res) => {
 
+    
+
     const { name, password, email, role} = req.body;
     const user = new User({ name, password, email, role });
     
-    //verificar si el correo existe
+    //Verificar si el correo existe
+    const existEmail = await User.findOne({ email });
+    if( existEmail ){
+        return res.status(400).json({
+            msg : 'Ese correo ya existe!'
+        })
+    }
 
-    //encriptar la contraseña
+    //Encriptar la contraseña
     const salt = bcrypt.genSaltSync();
     user.password = bcrypt.hashSync(password, salt);
     
-    
+    //Guardar en la base de datos
     await user.save();
 
     res.json({
@@ -43,7 +52,6 @@ const postUsers = async(req, res) => {
 
 const deleteUsers = (req, res) => {
     res.json({
-
         name: 'delete api - controller'
     });
 }
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { getUsers, putUsers, postUsers, deleteUsers, patchUsers } = require('../controller/users.controller');
 const router = Router();
 
@@ -6,7 +8,13 @@ router.get('/', getUsers);
 
 router.put('/:id', putUsers);
 
-router.post('/', postUsers);
+router.post('/', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe contener al menos 5 caracteres').isLength({ min: 6}),
+    check('email', 'El correo no es valido').isEmail(),
+    check('role', 'No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    validarCampos
+], postUsers);
 
 router.delete('/', deleteUsers);
 
